ec3970f6b8845d0345a6267fef192cf4ec52996b
Abdelrahman Alaa <abdelrahmanalaa107@gmail.com>
2025-07-28 23:40:29 +0300

    seed some testing data

--- Data/AppDbContext.cs
+++ Data/AppDbContext.cs
@@ -14,6 +14,9 @@ public class AppDbContext : DbContext
 
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
+        base.OnModelCreating(modelBuilder);
+       
+        
         modelBuilder.Entity<Department>()
             .HasIndex(d => d.Name).IsUnique();
         modelBuilder.Entity<Department>()
@@ -24,5 +27,21 @@ public class AppDbContext : DbContext
 
         modelBuilder.Entity<AttendanceRecord>()
             .HasIndex(a => new { a.EmployeeId, a.Date }).IsUnique();
+
+
+        modelBuilder.Entity<Department>().HasData(
+            new Department { Id = 1, Name = "HR Management", Code = "HRMG", Location = "Cairo" },
+            new Department { Id = 2, Name = "Tech Team", Code = "TECH", Location = "Alexandria" }
+        );
+
+        modelBuilder.Entity<Employee>().HasData(
+            new Employee { Id = 1, Code = 1001, FullName = "Ali Hassan Omar Mostafa", Email = "ali@company.com", DepartmentId = 1 },
+            new Employee { Id = 2, Code = 1002, FullName = "Sara Ahmed Hossam Khaled", Email = "sara@company.com", DepartmentId = 2 }
+        );
+
+        modelBuilder.Entity<AttendanceRecord>().HasData(
+            new AttendanceRecord { Id = 1, EmployeeId = 1, Date = DateTime.Today.AddDays(-1), Status = AttendanceStatus.Present },
+            new AttendanceRecord { Id = 2, EmployeeId = 2, Date = DateTime.Today.AddDays(-1), Status = AttendanceStatus.Absent }
+        );
     }
 }
