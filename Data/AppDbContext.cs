798ce20937581df351907591885a35dbfb8fb407
Abdelrahman Alaa <abdelrahmanalaa107@gmail.com>
2025-07-28 23:04:27 +0300

    create app dbContext

---
+++ Data/AppDbContext.cs
@@ -0,0 +1,28 @@
+﻿using Employee_Tracker.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Employee_Attendance_Tracker.Data;
+
+public class AppDbContext : DbContext
+{
+    public AppDbContext(DbContextOptions<AppDbContext> options)
+        : base(options) { }
+
+    public DbSet<Department> Departments { get; set; }
+    public DbSet<Employee> Employees { get; set; }
+    public DbSet<AttendanceRecord> AttendanceRecords { get; set; }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<Department>()
+            .HasIndex(d => d.Name).IsUnique();
+        modelBuilder.Entity<Department>()
+            .HasIndex(d => d.Code).IsUnique();
+
+        modelBuilder.Entity<Employee>()
+            .HasIndex(e => e.Email).IsUnique();
+
+        modelBuilder.Entity<AttendanceRecord>()
+            .HasIndex(a => new { a.EmployeeId, a.Date }).IsUnique();
+    }
+}
